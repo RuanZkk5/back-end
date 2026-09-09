72637b2f6ec87fda63356ad479bc76206419abad
RUAN VICTOR DEL BIANCO CARVALHO <ruan.c@edu.senai.br>
2026-09-09 08:52:05 -0300

    Add files via upload

---
+++ Ex6.php
@@ -0,0 +1,12 @@
+<?php
+
+$qnt = 0;
+
+for ($num = 1; $num <= 50; $num++) {
+    if ($num % 3 == 0) {
+        echo "mutiplo de 3: $num <br>";
+    $qnt++;
+
+    }
+}
+    echo"total de multiplos de 3: $qnt";  
